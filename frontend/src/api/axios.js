95076c33963760c0b398bc61ccfa5d392c7fcccf
Fausto-Andrade <faustoantonioandradevera@gmail.com>
2026-04-07 22:03:11 -0500

    fix: base url de axios corregida

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', // La URL de tu backend
+  // baseURL: 'http://localhost:3000/api'
+  baseURL: 'http://138.36.237.111:3000/api'
 });
 
 export default api;
